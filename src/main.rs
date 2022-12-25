0dd10362f3c47f410fb7d065278be7f09e821b75
adwd <ecdt.mn@gmail.com>
2022-12-25 22:34:08 +0900

    add sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::postgres::PgPoolOptions;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://world:world123@localhost:7777/world-db")
+        .await?;
+
+    let x = sqlx::query!(r#"select * from public.city limit 10;"#)
+        .fetch_all(&pool)
+        .await?;
+
+    println!("{:?}", x);
+
+    Ok(())
 }
