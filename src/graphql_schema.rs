b012d9928bbb5863eb98ca8fafc7bc1712c5e365
adwd <ecdt.mn@gmail.com>
2022-12-27 21:54:24 +0900

    add countries query

---
+++ src/graphql_schema.rs
@@ -0,0 +1,56 @@
+use async_graphql::{http::GraphiQLSource, *};
+use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Extension, Router,
+};
+use sqlx::{Pool, Postgres};
+
+#[derive(SimpleObject, Debug)]
+pub struct Country {
+    pub code: String,
+    pub name: String,
+}
+
+pub struct Query;
+
+#[Object]
+impl Query {
+    /// Returns the sum of a and b
+    async fn add(&self, a: i32, b: i32) -> i32 {
+        a + b
+    }
+
+    async fn countries(&self, ctx: &Context<'_>) -> Result<Vec<Country>> {
+        let pool = ctx.data_unchecked::<Pool<Postgres>>();
+        let countries = sqlx::query_as!(Country, "select code, name from public.country limit 10;")
+            .fetch_all(pool)
+            .await?;
+
+        Ok(countries)
+    }
+}
+
+async fn graphql_handler(
+    schema: Extension<Schema<Query, EmptyMutation, EmptySubscription>>,
+    req: GraphQLRequest,
+) -> GraphQLResponse {
+    schema.execute(req.into_inner()).await.into()
+}
+
+async fn graphiql() -> impl IntoResponse {
+    Html(GraphiQLSource::build().endpoint("/graphql").finish())
+}
+
+pub fn router(pool: &Pool<Postgres>) -> Router {
+    let schema = Schema::build(Query, EmptyMutation, EmptySubscription)
+        .data(pool.clone())
+        .finish();
+
+    println!("GraphiQL IDE: http://localhost:3000");
+
+    Router::new()
+        .route("/graphql", get(graphiql).post(graphql_handler))
+        .layer(Extension(schema))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,6 @@
-use async_graphql::{http::GraphiQLSource, *};
-use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
-use axum::{
-    response::{Html, IntoResponse},
-    routing::get,
-    Extension, Router,
-};
+use axum::{routing::get, Router};
+use axum_graphql_example::graphql_schema;
 use sqlx::postgres::PgPoolOptions;
-
-struct Query;
-
-#[Object]
-impl Query {
-    /// Returns the sum of a and b
-    async fn add(&self, a: i32, b: i32) -> i32 {
-        a + b
-    }
-}
-async fn graphql_handler(
-    schema: Extension<Schema<Query, EmptyMutation, EmptySubscription>>,
-    req: GraphQLRequest,
-) -> GraphQLResponse {
-    schema.execute(req.into_inner()).await.into()
-}
-
-async fn graphiql() -> impl IntoResponse {
-    Html(GraphiQLSource::build().endpoint("/graphql").finish())
-}
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
@@ -34,23 +9,10 @@ async fn main() -> Result<(), sqlx::Error> {
         .connect("postgres://world:world123@localhost:7777/world-db")
         .await?;
 
-    let x = sqlx::query!(r#"select * from public.city limit 10;"#)
-        .fetch_all(&pool)
-        .await?;
-
-    println!("{:?}", x);
-
-    let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
-    let res = schema.execute("{ add(a: 10, b: 20) }").await;
-    println!("{:?}", res);
-
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/graphql", get(graphiql).post(graphql_handler))
-        .layer(Extension(schema));
-
-    println!("GraphiQL IDE: http://localhost:3000");
+        .merge(graphql_schema::router(&pool));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
