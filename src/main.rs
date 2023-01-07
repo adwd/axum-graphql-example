6afbe646666eb19fea3d0cc110f009d217246660
adwd <ecdt.mn@gmail.com>
2023-01-07 22:44:42 +0900

    fix CI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use axum::{routing::get, Router};
 use axum_graphql_example::graphql_schema;
+use dotenv::from_filename;
 use sqlx::postgres::PgPoolOptions;
 use std::env;
 
@@ -8,9 +9,22 @@ async fn main() -> Result<(), sqlx::Error> {
     env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
+    match env::var("CI").ok() {
+        Some(_) => {
+            // CIのときはPostgreSQLを立てないのでsqlxがsqlx-data.jsonを使うようにする
+            // 環境変数にDATABASE_URLがあるとそれを見に行ってしまうのでDATABASE_URLがない.envファイルを読み込む
+            from_filename(".ci.env").ok();
+        }
+        None => {
+            from_filename(".env").ok();
+        }
+    }
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect("postgres://world:world123@localhost:7777/world-db")
+        .connect(&database_url)
         .await?;
 
     // build our application with a single route
