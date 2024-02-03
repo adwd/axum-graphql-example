99274c2f010b4dce69cb4ae503e8d12058260668
adwd <adwd@users.noreply.github.com>
2024-02-04 00:37:24 +0900

    remove integration test

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,8 @@ async fn main() -> Result<(), sqlx::Error> {
         .route("/", get(|| async { "Hello, World!" }))
         .merge(graphql_schema::router(&pool));
 
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 
     Ok(())
 }
